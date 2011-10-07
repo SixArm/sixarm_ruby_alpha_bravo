5991dc6643f659bc5c65a3663b4dcbbc03161bd5
SixArm <sixarm@sixarm.com>
2011-10-06 21:53:50 -0700

    Updates for gem publishing

--- lib/sixarm_ruby_alpha_bravo.rb
+++ lib/sixarm_ruby_alpha_bravo.rb
@@ -1,55 +1,6 @@
 # -*- coding: utf-8 -*-
-
 =begin rdoc
-
-= SixArm.com » Ruby » AlphaBravo class provides lookup for standard alphabet keywords
-
-Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
-Copyright:: Copyright (c) 2006-2011 Joel Parker Henderson
-License:: See LICENSE.txt file
-
-Alpha Bravo provides a lookup for international standard alphabet keywords.
-
-For example, A is alpha, B is bravo, C is charlie, D is delta, etc.
-
-Example:
-  AlphaBravo.list[0] => 'alpha'
-  AlphaBravo.list[1] => 'bravo'
-  AlphaBravo.list[2] => 'charlie'
-
-Example
-  AlphaBravo.hash['a'] => 'alpha'
-  AlphaBravo.hash['b'] => 'bravo'
-  AlphaBravo.hash['c'] => 'charlie'
-
-The complete set is:
-  alpha
-  bravo
-  charlie
-  delta
-  echo
-  foxtrot
-  golf
-  hotel
-  india
-  juliet
-  kilo
-  lima
-  mike
-  november
-  oscar
-  papa
-  quebec
-  romeo
-  sierra
-  tango
-  uniform
-  victor
-  whiskey
-  xray
-  yankee
-  zulu
-
+Please see README.rdoc
 =end
 
 
